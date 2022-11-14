d588ede792d11c4f473b7a5ea7ab6bfccbd007ca
joshua9154 <44042301+joshua9154@users.noreply.github.com>
2022-11-14 11:45:51 -0500

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,6 +40,7 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    @game.guess_several_letters(@game,guess)
     redirect '/show'
   end
   
